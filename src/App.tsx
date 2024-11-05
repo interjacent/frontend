1892191819a99d76480af828215304d8525654b0
DarkHole1 <dark.hole1@yandex.ru>
2024-11-05 21:11:09 +0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Interjacent project</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/mvp" element={"Mococo Very Pretty"} />
+          <Route path="*" element={<h1>Interjacent project</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
