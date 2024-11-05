147d16cafecb241febe3b7822e56f47377f079f1
rsemenov <rsemenov@solanteq.com>
2024-11-05 16:22:44 +0300

    dist: add basic layout for project

---
+++ interjacent/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Interjacent project</h1>
+    </div>
+  );
+}
+
+export default App;
