78b2faf561da29dc59483df62a4c5227b86cb380
DarkHole1 <dark.hole1@yandex.ru>
2024-11-06 14:58:14 +0300

    Add main page

--- src/Components/Pages/Main.tsx
+++ src/Components/Pages/Main.tsx
@@ -1,1 +1,22 @@
-export const Main = () => <h1>Interjacent project</h1>;
+import "./Main.css";
+
+export const Main = () => {
+  return (
+    <div className="container-center">
+      <div className="main-page">
+        <div className="logo">interjacent</div>
+        <div>Мы готовы провести встречу в</div>
+        <div className="checkboxes">
+          <span>Пн</span>
+          <span>Вт</span>
+          <span>Ср</span>
+          <span>Чт</span>
+          <span>Пт</span>
+          <span>Сб</span>
+          <span>Вс</span>
+        </div>
+        <div>с 8:00 до 23:00</div>
+      </div>
+    </div>
+  );
+};
