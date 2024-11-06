a4d833696ffdd78fc79d3bcb41f9d13865d00ecc
rsemenov <rsemenov@solanteq.com>
2024-11-06 18:02:57 +0300

    feat: add final page

--- src/Components/Pages/Final.tsx
+++ src/Components/Pages/Final.tsx
@@ -1,1 +1,17 @@
-export const Final = () => <>"Final page"</>;
+import './Final.css';
+import {PropsWithChildren} from "react";
+
+type Props = PropsWithChildren<{
+  dayOfWeek: string;
+  startTime: Date;
+  endTime: Date;
+}>;
+
+export const Final = () => <div className="final-component">
+  <h1 className="username">Фырфырчик #a1b24c</h1>
+
+  <div className="final-message">
+    <p>Встреча состоится <br/> во вт 14:00 - 15:30</p>
+    <p className="final-good-luck">удачки</p>
+  </div>
+</div>;
